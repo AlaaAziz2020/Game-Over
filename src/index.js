ffbd3c998e1b8a007c98b36586814ce1a33e2583
alaaaziz2020 <alaaaziz017@gmail.com>
2023-02-12 21:21:48 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import './index.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'jquery/dist/jquery.min.js';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
